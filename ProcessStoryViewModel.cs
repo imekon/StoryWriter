6c3396e94edcfe491e0e0c68874cb1b04402e3fd
Pete Goodwin <imekon@users.noreply.github.com>
2022-09-23 22:37:38 +0100

    importing stories

---
+++ ProcessStoryViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Controls;
+
+namespace StoryWriter
+{
+    internal class ProcessStoryViewModel : ViewModelBase
+    {
+        private Story story;
+
+        public ProcessStoryViewModel(Story story)
+        {
+            this.story = story;
+        }
+
+        public string Title
+        {
+            get => story.Title;
+            set
+            {
+                story.Title = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+
+        public string Text
+        {
+            get => story.Text;
+        }
+
+        public int WordCount => story.WordCount;
+    }
+}
--- ProcessWindowViewModel.cs
+++ ProcessWindowViewModel.cs
@@ -1,18 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows.Input;
 
 namespace StoryWriter
 {
     internal class ProcessWindowViewModel : ViewModelBase
     {
         private string processPath;
+        private ObservableCollection<ProcessStoryViewModel> processStories;
 
         public ProcessWindowViewModel()
         {
             processPath = @"F:\stories";
+            processStories = new ObservableCollection<ProcessStoryViewModel>();
         }
 
         public string ProcessPath
@@ -24,5 +24,26 @@ namespace StoryWriter
                 OnPropertyChanged(nameof(ProcessPath));
             }
         }
+
+        public ICommand ProcessCommand
+        {
+            get
+            {
+                return new DelegateCommand((o) =>
+                {
+                    var files = Directory.GetFiles(processPath, "*.md");
+                    foreach(var filename in files)
+                    {
+                        var story = new Story();
+                        story.Title = Path.GetFileNameWithoutExtension(filename);
+                        story.Text = File.ReadAllText(filename);
+                        var storyViewModel = new ProcessStoryViewModel(story);
+                        processStories.Add(storyViewModel);
+                    }
+                });
+            }
+        }
+
+        public ObservableCollection<ProcessStoryViewModel> ProcessStories => processStories;
     }
 }
